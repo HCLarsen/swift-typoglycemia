5bf0062e94a98a49a224794e0c364d9933182585
Chris Larsen <clarsenipod@gmail.com>
2016-12-05 17:26:23 -0500

    Initial Commit

---
+++ typoglycemia/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  typoglycemia
+//
+//  Created by Chris Larsen on 2016-12-05.
+//  Copyright © 2016 Larsen Tech. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
